f813ce3bc67bebab1ccc64c7e790032bbb79840b
Dhruv1249 <dhr1249.lm@gmail.com>
2025-10-04 01:19:17 +0530

    Dark mode fixed

--- src/app/map2/page.tsx
+++ src/app/map2/page.tsx
@@ -12,13 +12,8 @@ type AppConfig = {
   mapId: string;
 };
 
-const mapStyles: { [key: string]: google.maps.MapTypeStyle[] } = {
-  dark: [ { elementType: "geometry", stylers: [{ color: "#242f3e" }] }, { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] }, { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] }, { featureType: "administrative.locality", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] }, { featureType: "poi", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] }, { featureType: "poi.park", elementType: "geometry", stylers: [{ color: "#263c3f" }] }, { featureType: "poi.park", elementType: "labels.text.fill", stylers: [{ color: "#6b9a76" }] }, { featureType: "road", elementType: "geometry", stylers: [{ color: "#38414e" }] }, { featureType: "road", elementType: "geometry.stroke", stylers: [{ color: "#212a37" }] }, { featureType: "road", elementType: "labels.text.fill", stylers: [{ color: "#9ca5b3" }] }, { featureType: "road.highway", elementType: "geometry", stylers: [{ color: "#746855" }] }, { featureType: "road.highway", elementType: "geometry.stroke", stylers: [{ color: "#1f2835" }] }, { featureType: "road.highway", elementType: "labels.text.fill", stylers: [{ color: "#f3d19c" }] }, { featureType: "transit", elementType: "geometry", stylers: [{ color: "#2f3948" }] }, { featureType: "transit.station", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] }, { featureType: "water", elementType: "geometry", stylers: [{ color: "#17263c" }] }, { featureType: "water", elementType: "labels.text.fill", stylers: [{ color: "#515c6d" }] }, { featureType: "water", elementType: "labels.text.stroke", stylers: [{ color: "#17263c" }] } ],
-  light: []
-};
 
 export default function Map2Page(): React.ReactElement {
-  const position = { lat: 32.1036, lng: 76.2711 }; // Kangra
   const [config, setConfig] = useState<AppConfig | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -51,11 +46,10 @@ export default function Map2Page(): React.ReactElement {
       <div style={{ height: "100dvh", width: "100%", position: "relative" }}>
         <NavBar />
         <GoogleMap
-          defaultCenter={position}
           defaultZoom={10}
           gestureHandling="greedy"
           mapId={config.mapId}
-          styles={darkMode ? mapStyles.dark : mapStyles.light}
+          colorScheme={darkMode ? "DARK": "LIGHT"}
           restriction={{ latLngBounds: { north: 85, south: -85, west: -179, east: 179 }, strictBounds: true }}
         >
           <DrawingTools 
